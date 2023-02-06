52d057d5ff9f20336b1e68a7255e5b5cba0b63ac
Bao <baothaitruong2000@gmail.com>
2023-02-06 19:37:25 +0700

    feat: user info page update date_time successfull

--- src/pages/User/components/DateSelect/DateSelect.tsx
+++ src/pages/User/components/DateSelect/DateSelect.tsx
@@ -1,17 +1,34 @@
 import _ from 'lodash'
+import { useState } from 'react'
 
 interface dateDrop {
   value: string
+  onChange: (value: Date) => void
 }
 
-function DateSelect({ value }: dateDrop) {
-  const date = new Date(value)
+function DateSelect({ value, onChange }: dateDrop) {
+  const valueDate = new Date(value)
+  let newDate = {
+    year: valueDate.getFullYear(),
+    month: valueDate.getMonth(),
+    date: valueDate.getDate()
+  }
 
+  const handleChangeTime = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value, name } = e.currentTarget
+    newDate = {
+      ...newDate,
+      [name]: Number(value)
+    }
+    onChange(new Date(newDate.year, newDate.month, newDate.date))
+  }
   return (
     <div className='flex flex-grow gap-[20px]'>
       <select
         className='w-full rounded-[2px] border py-[10px] px-[15px] text-[14px] focus:outline-gray-400'
-        value={date.getDate()}
+        value={newDate.date}
+        name='date'
+        onChange={handleChangeTime}
       >
         <option disabled>Ngày</option>
         {_.range(1, 32).map((e) => {
@@ -24,7 +41,9 @@ function DateSelect({ value }: dateDrop) {
       </select>
       <select
         className='w-full rounded-[2px] border py-[10px] px-[15px] text-[14px] focus:outline-gray-400'
-        value={date.getMonth() + 1}
+        value={newDate.month}
+        name='month'
+        onChange={handleChangeTime}
       >
         <option disabled>Tháng</option>
         {_.range(0, 12).map((e) => {
@@ -37,7 +56,9 @@ function DateSelect({ value }: dateDrop) {
       </select>
       <select
         className='w-full rounded-[2px] border py-[10px] px-[15px] text-[14px] focus:outline-gray-400'
-        value={date.getFullYear()}
+        value={newDate.year}
+        name='year'
+        onChange={handleChangeTime}
       >
         <option disabled>Năm</option>
         {_.range(1990, new Date().getFullYear() + 1).map((e) => {
